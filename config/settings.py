a25529660bf66f826290e1de8adf79e743015af8
dearsecret <dearsecret1217@gmail.com>
2022-08-25 01:59:50 +0900

    core model

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ DJANGO_APPS = [
 ]
 PROJECT_APPS = [
     "users.apps.UsersConfig",
+    "core.apps.CoreConfig",
+    "rooms.apps.RoomsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TimeStampedModel(models.Model):
+    created = models.TimeField(auto_now_add=True)
+    updated = models.TimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
